85978f9fc585e23fcc54cf12e71b274b3b477f41
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-04-01 02:59:47 +0530

    Small change

--- anti_hacking.py
+++ anti_hacking.py
@@ -5,13 +5,10 @@ from string import ascii_letters, digits
 
 def main():
 
-	#while True:
+	#while true:
+		text = '+'.join(choice(ascii_letters+digits) for i in range(100))
 
-	#time.sleep(3600)
-
-	text = '+'.join(choice(ascii_letters+digits) for i in range(10))
-
-	keyboard.press_and_release(text)
+		keyboard.press_and_release(text)
 
 if __name__ == '__main__':
 	main()
